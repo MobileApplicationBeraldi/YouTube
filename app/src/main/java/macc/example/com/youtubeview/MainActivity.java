dcddeb06f4412d87d74095fcd9f17d0519c5a879
RBeraldi <beraldi@dis.uniroma1.it>
2017-11-03 09:13:39 +0100

    Initial commit

---
+++ app/src/main/java/macc/example/com/youtubeview/MainActivity.java
@@ -0,0 +1,52 @@
+package macc.example.com.youtubeview;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/*
+ * Copyright 2012 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+/**
+ * A simple YouTube Android API demo application which shows how to create a simple application that
+ * displays a YouTube Video in a {@link YouTubePlayerView}.
+ * <p>
+ * Note, to use a {@link YouTubePlayerView}, your activity must extend {@link YouTubeBaseActivity}.
+ */
+public class MainActivity extends YouTubeBaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String youTubeMovieKey = "SUXWAEX2jlg";
+        String youTubeDeveloperKey = "";
+
+        Intent intent = YouTubeStandalonePlayer.createVideoIntent(this,youTubeDeveloperKey,youTubeMovieKey);
+        startActivity(intent);
+    }
+
+}
